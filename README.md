e59f8e53e21e77aefcc1de567802c7868d1ac63b
valik <valentin.pazushko@tut.by>
2021-11-09 21:37:33 +0300

    Update

---
+++ README.md
@@ -0,0 +1,1 @@
+My lab 4 solution is [here](https://github.com/AlekseiBalabko/react-news-lab)
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component{
+    render(){
+        return(
+            <header className="app__header">
+                <h1 className="app__header-text">News</h1>
+            </header>
+        );
+    }
+}
